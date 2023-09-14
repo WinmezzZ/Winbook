4ed245b7555264053d7648d29e3b1748362b9d6a
Winme <winme0308@gmail.com>
2023-09-14 23:16:31 +0800

    feat: 本地服务器

--- src/pages/mall/index.tsx
+++ src/pages/mall/index.tsx
@@ -1,9 +1,31 @@
+import React, { useEffect } from 'react';
 import { Text, View } from 'react-native';
+// import {NetworkInfo} from 'react-native-network-info';
+import RNFS from 'react-native-fs';
+import StaticServer from 'react-native-static-server';
 
 export function MallPage() {
+  useEffect(() => {
+    const path = RNFS.DocumentDirectoryPath;
+
+    const server = new StaticServer(12345, path, { keepAlive: true });
+
+    console.log('serverserverserverserverserverserverserver', server);
+
+    server.start().then(url => {
+      console.log('Serving at URL', url);
+    });
+
+    // 获取设备的IP地址
+
+    return () => {
+      server.stop();
+    };
+  }, []);
+
   return (
     <View>
-      <Text>书城</Text>
+      <Text>WLAN Transfer</Text>
     </View>
   );
 }
